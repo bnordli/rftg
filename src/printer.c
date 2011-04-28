87631ab4c3d673634f1b3c214d1c7381bcd5df89
B. Nordli <bnordli@gmail.com>
2011-04-28 22:28:39 +0200

    Use substitute file in printer.

--- src/printer.c
+++ src/printer.c
@@ -25,12 +25,17 @@
 /*
  * Print messages?
  */
-int verbose = 0;
+static int verbose = 0;
 
 /*
  * Player we're playing as.
  */
-int player_us = 0;
+static int player_us = 0;
+
+/*
+ * Number of cards before newline separator.
+ */
+static int cards_per_line = 1;
 
 /*
  * Default player names.
@@ -44,6 +49,16 @@ static char *player_names[MAX_PLAYER] =
 	"Cyan",
 	"Purple",
 };
+
+/*
+ * Substite card file.
+ */
+static char *subs_file = NULL;
+
+/*
+ * Substitute for card names.
+ */
+static char substitutes[MAX_DESIGN][64];
 
 /*
  * Print messages to standard output.
@@ -89,24 +104,33 @@ static void printer_notify_rotation(game *g, int who)
  */
 static void print_reverse(game *g, int x)
 {
-	int p;
+	int n, p;
 	int cards[MAX_DECK];
 
 	/* Loop over cards */
-	for (p = 0 ; x != -1; x = g->deck[x].next, ++p)
+	for (n = 0 ; x != -1; x = g->deck[x].next, ++n)
 	{
 		/* Save card */
-		cards[p] = x;
+		cards[n] = x;
 	}
 
-	/* Decrease pointer by one */
-	p--;
-
 	/* Traverse cards in reverse order */
-	for (; p >= 0; --p)
+	for (p = 1; p <= n; ++p)
 	{
-		/* Print card name */
-		printf("%s\n", g->deck[cards[p]].d_ptr->name);
+		/* Check for substitutes */
+		if (subs_file)
+		{
+			/* Print substitute */
+			printf("%s ", substitutes[g->deck[cards[n - p]].d_ptr->index]);
+		}
+		else
+		{
+			/* Print card name */
+			printf("%s ", g->deck[cards[n - p]].d_ptr->name);
+		}
+
+		/* Print newline if needed */
+		if (p % cards_per_line == 0) printf("\n");
 	}
 }
 
@@ -145,7 +169,7 @@ static void print_game(game *g, int who)
 }
 
 /*
- * Printer choice: Just end the game.
+ * Choice function: Just end the game.
  */
 static void printer_make_choice(game *g, int who, int type, int list[], int *nl,
                                 int special[], int *ns, int arg1, int arg2, int arg3)
@@ -153,7 +177,6 @@ static void printer_make_choice(game *g, int who, int type, int list[], int *nl,
 	/* Set game over */
 	g->game_over = 1;
 }
-
 
 /*
  * Private message.
@@ -182,6 +205,109 @@ decisions text_func =
 };
 
 /*
+ * Read substitutes file.
+ */
+static void read_subs()
+{
+	FILE *fff;
+	char buf[1024];
+	char *p;
+	int card_found, i;
+
+	/* Clear all substitute strings */
+	for (i = 0; i < MAX_DESIGN; ++i)
+		substitutes[i][0] = '\0';
+
+	/* Open file for reading */
+	fff = fopen(subs_file, "r");
+
+	/* Check for failure */
+	if (!fff)
+	{
+		/* Print error and exit */
+		printf("Could not open substitute file %s.\n", subs_file);
+		exit(1);
+	}
+
+	/* Read first line */
+	fgets(buf, 1024, fff);
+
+	/* Set cards per line */
+	cards_per_line = atoi(buf);
+
+	/* Check for failure */
+	if (cards_per_line == 0)
+	{
+		/* Print error and exit */
+		printf("Could not parse first line "
+		       "of substitute file as non-zero integer:\n%s\n", buf);
+		exit(1);
+	}
+
+	/* Read lines */
+	while (fgets(buf, 1024, fff))
+	{
+		/* Strip newline */
+		if (buf[strlen(buf) - 1] == '\n') buf[strlen(buf) - 1] = '\0';
+
+		/* Locate separator */
+		p = strchr(buf, ';');
+
+		/* Check for separator not found */
+		if (!p)
+		{
+			/* Print error and exit */
+			printf("Could not find separator (;) in line %s.\n", buf);
+			exit(1);
+		}
+
+		/* End card name */
+		*p = '\0';
+
+		/* Move to start of substutite string */
+		++p;
+
+		/* Clear card found flag */
+		card_found = 0;
+
+		/* Loop over all designs */
+		for (i = 0; i < MAX_DESIGN; ++i)
+		{
+			/* Check for matching card name */
+			if (!strcmp(buf, library[i].name))
+			{
+				/* Copy substitute string */
+				strcpy(substitutes[i], p);
+
+				/* Remember that card is found */
+				card_found = 1;
+			}
+		}
+
+		/* Check whether card was found */
+		if (!card_found)
+		{
+			/* Print error and exit */
+			printf("Could not recognize card name %s.\n", buf);
+			exit(1);
+		}
+	}
+
+	/* Loop over all designs */
+	for (i = 0; i < MAX_DESIGN; ++i)
+	{
+		/* Check for empty substitute */
+		if (strlen(substitutes[i]) == 0)
+		{
+			/* Print error and exit */
+			printf("Did not find substitute string for %s.\n",
+				library[i].name);
+			exit(1);
+		}
+	}
+}
+
+/*
  * Load a game and simulate until end of choice log.
  */
 int main(int argc, char *argv[])
@@ -189,7 +315,7 @@ int main(int argc, char *argv[])
 	game my_game;
 	int i;
 	char buf[1024];
-	char *fname = NULL;
+	char *save_file = NULL;
 
 	/* Set random seed */
 	my_game.random_seed = time(NULL);
@@ -211,27 +337,33 @@ int main(int argc, char *argv[])
 			verbose++;
 		}
 
-		/* Check for debug */
-		if (!strcmp(argv[i], "-d"))
+		/* Check for substitutes */
+		if (!strcmp(argv[i], "-s"))
 		{
-			/* Wait some time */
-			for (i = 0; i < 2000000000; ++i);
+			/* Set substitute file name */
+			subs_file = argv[++i];
 		}
 
 		/* Default argument is path to saved game */
-		else if (!fname)
+		else if (!save_file)
 		{
 			/* Set file name */
-			fname = argv[i];
+			save_file = argv[i];
 		}
 	}
 
 	/* Check for missing file name */
-	if (!fname)
+	if (!save_file)
 	{
 		/* Print error and exit */
 		printf("No save file supplied! (use -s)");
 		exit(1);
+	}
+
+	/* Check for substitute file requested */
+	if (subs_file)
+	{
+		read_subs();
 	}
 
 	/* Initialize players */
@@ -248,10 +380,10 @@ int main(int argc, char *argv[])
 	}
 
 	/* Try to load savefile into load state */
-	if (load_game(&my_game, fname) < 0)
+	if (load_game(&my_game, save_file) < 0)
 	{
 		/* Print error and exit */
-		printf(buf, "Failed to load game from file %s\n", fname);
+		printf(buf, "Failed to load game from file %s\n", save_file);
 		exit(1);
 	}
 
