ab4a86c74801789945eb4b3dc20d308781cf174c
B. Nordli <bnordli@gmail.com>
2011-10-15 01:27:12 +0200

    Export messages in replay

--- src/replay.c
+++ src/replay.c
@@ -67,7 +67,7 @@ static int original_id(int who)
 typedef struct message
 {
 	/* The text of the message */
-	char text[150];
+	char text[256];
 	
 	/* The format of the message */
 	char* format;
@@ -214,6 +214,15 @@ static void export_callback(FILE *fff, int orig_who)
 	fputs("  </Links>\n", fff);
 }
 
+/* The current message */
+static char msg[256];
+
+/* The current number of special cards in an export */
+static int num_special_cards;
+
+/* The current special cards in an export */
+static card *special_cards[20];
+
 /* Export the game seen from a specific player */
 static void export(game *g, int who)
 {
@@ -229,7 +238,8 @@ static void export(game *g, int who)
 	
 	/* Export game to file */
 	if (export_game(g, filename, export_style_sheet, server_name,
-	    who, NULL, 0, NULL, export_log, export_callback, orig_who) < 0)
+	    who, msg, num_special_cards, special_cards,
+	    export_log, export_callback, orig_who) < 0)
 	{
 		/* Log error */
 		printf("Could not export game to %s\n", filename);
@@ -242,21 +252,314 @@ static void export(game *g, int who)
 }
 
 /*
+ * Choose a card to place for the Develop or Settle phases.
+ */
+static void choose_place(game *g, int who, int list[], int num, int phase,
+                         int special)
+{
+	int allow_takeover = (phase == PHASE_SETTLE);
+
+	/* Create prompt */
+	sprintf(msg, "Choose card to %s",
+	        phase == PHASE_DEVELOP ? "develop" : "settle");
+
+	/* Check for special card used to provide power */
+	if (special != -1)
+	{
+		/* Append name to prompt */
+		strcat(msg, " using ");
+		strcat(msg, g->deck[special].d_ptr->name);
+
+		/* XXX Check for "Rebel Sneak Attack" */
+		if (!strcmp(g->deck[special].d_ptr->name, "Rebel Sneak Attack"))
+		{
+			/* Takeover not allowed */
+			allow_takeover = 0;
+		}
+	}
+
+	/* Check for settle phase and possible takeover */
+	if (allow_takeover && settle_check_takeover(g, who, NULL, 0))
+	{
+		/* Append takeover information */
+		strcat(msg, " (or pass if you want to perform a takeover)");
+	}
+}
+
+/*
+ * Choose method of payment for a placed card.
+ *
+ * We include some active cards that have powers that can be triggered,
+ * such as the Contact Specialist or Colony Ship.
+ */
+static void choose_pay(game *g, int who, int which, int list[], int *num,
+                       int special[], int *num_special, int mil_only)
+{
+	card *c_ptr;
+
+	/* Get card we are paying for */
+	c_ptr = &g->deck[which];
+
+	/* Create prompt */
+	sprintf(msg, "Choose payment for %s ", c_ptr->d_ptr->name);
+}
+
+/*
  * Store the player's next choice.
  */
 static void replay_make_choice(game *g, int who, int type, int list[], int *nl,
                                int special[], int *ns, int arg1, int arg2,
                                int arg3)
 {
-	int current, next, orig_who;
+	int i, current, next, orig_who;
 
 	/* Compute the original player seat */
 	orig_who = original_id(who);
 
-	/* TODO: message and special cards */
+	/* Determine type of choice */
+	switch (type)
+	{
+		/* Action(s) to play */
+		case CHOICE_ACTION:
+
+			/* Check for advanced game */
+			if (g->advanced)
+			{
+				/* Check for two actions */
+				if (arg1 == 0)
+				{
+					/* Create prompt */
+					sprintf(msg, "Choose Actions");
+				}
+				/* Check for only first action */
+				else if (arg1 == 1)
+				{
+					/* Create prompt */
+					sprintf(msg, "Choose first Action");
+				}
+				/* Check for only second action */
+				else if (arg1 == 2)
+				{
+					/* Create prompt */
+					sprintf(msg, "Choose second Action");
+				}
+			}
+			else
+			{
+				/* Create prompt */
+				sprintf(msg, "Choose action");
+			}
+
+			break;
+
+		/* Start world */
+		case CHOICE_START:
+
+			/* Save special cards */
+			num_special_cards = *ns;
+			for (i = 0; i < *ns; ++i) special_cards[i] = &g->deck[special[i]];
+
+			/* Create prompt */
+			sprintf(msg, "Choose start world and hand discards");
+			break;
+
+		/* Discard */
+		case CHOICE_DISCARD:
+
+			/* Create prompt */
+			sprintf(msg, "Choose %d card%s to discard", arg1, PLURAL(arg1));
+			break;
+
+		/* Save a card under a world for later */
+		case CHOICE_SAVE:
+
+			/* Save special cards */
+			num_special_cards = *nl;
+			for (i = 0; i < *nl; ++i) special_cards[i] = &g->deck[list[i]];
+
+			/* Create prompt */
+			sprintf(msg, "Choose card to save for later");
+			break;
+
+		/* Choose to discard to gain prestige */
+		case CHOICE_DISCARD_PRESTIGE:
+
+			/* Create prompt */
+			sprintf(msg, "Choose card to discard for prestige");
+			break;
+
+		/* Place a development/world */
+		case CHOICE_PLACE:
+
+			/* Choose card to place */
+			choose_place(g, who, list, *nl, arg1, arg2);
+			break;
+
+		/* Pay for a development/world */
+		case CHOICE_PAYMENT:
+
+			/* Choose payment */
+			choose_pay(g, who, arg1, list, nl, special, ns, arg2);
+			break;
+
+		/* Choose a world to takeover */
+		case CHOICE_TAKEOVER:
+
+			/* Create prompt */
+			sprintf(msg, "Choose world to takeover and power to use");
+			break;
+
+		/* Choose a method of defense against a takeover */
+		case CHOICE_DEFEND:
+
+			/* Create prompt */
+			sprintf(msg, "Choose defense for %s (need %d extra military)",
+					g->deck[arg1].d_ptr->name, arg3 + 1);
+			break;
+
+		/* Choose whether to prevent a takeover */
+		case CHOICE_TAKEOVER_PREVENT:
+
+			/* Create prompt */
+			sprintf(msg, "Choose takeover to prevent");
+			break;
+
+		/* Choose world to upgrade with one from hand */
+		case CHOICE_UPGRADE:
+
+			/* Create prompt */
+			sprintf(msg, "Choose world to replace");
+			break;
+
+		/* Choose a good to trade */
+		case CHOICE_TRADE:
+
+			/* Create prompt */
+			sprintf(msg, "Choose good to trade%s", arg1 ? " (no bonuses)" : "");
+			break;
+
+		/* Choose a consume power to use */
+		case CHOICE_CONSUME:
+
+			/* Create prompt */
+			sprintf(msg, "Choose Consume power");
+			break;
+
+		/* Choose discards from hand for VP */
+		case CHOICE_CONSUME_HAND:
+
+			/* Check for prestige trade bonus power */
+			if (arg1 < 0)
+			{
+				/* Create prompt */
+				sprintf(msg, "Choose up to 2 cards to consume on Prestige Trade bonus");
+			}
+			else
+			{
+				/* Check for needing two cards */
+				if (g->deck[arg1].d_ptr->powers[arg2].code & P4_CONSUME_TWO)
+				{
+					/* Create prompt */
+					sprintf(msg, "Choose cards to consume on %s", g->deck[arg1].d_ptr->name);
+				}
+				else
+				{
+					/* Read power size */
+					i = g->deck[arg1].d_ptr->powers[arg2].times;
+					
+					/* Create prompt */
+					sprintf(msg, "Choose up to %d card%s to consume on %s",
+							i, PLURAL(i), g->deck[arg1].d_ptr->name);
+				}
+			}
+			break;
+
+		/* Choose good(s) to consume */
+		case CHOICE_GOOD:
+
+			/* Create prompt */
+			sprintf(msg, "Choose good%s to consume on %s",
+					arg1 == 1 && arg2 == 1 ? "" : "s", g->deck[special[0]].d_ptr->name);
+			break;
+
+		/* Choose lucky number */
+		case CHOICE_LUCKY:
+
+			/* Choose number */
+			sprintf(msg, "Choose Number");
+			break;
+
+		/* Choose card to ante */
+		case CHOICE_ANTE:
+
+			/* Create prompt */
+			sprintf(msg, "Choose card to ante");
+			break;
+
+		/* Choose card to keep in successful gamble */
+		case CHOICE_KEEP:
+
+			/* Create prompt */
+			sprintf(msg, "Choose card to keep");
+			break;
+
+		/* Choose windfall world to produce on */
+		case CHOICE_WINDFALL:
+
+			/* Create prompt */
+			sprintf(msg, "Choose windfall world to produce");
+			break;
+
+		/* Choose produce power to use */
+		case CHOICE_PRODUCE:
+
+			/* Create prompt */
+			sprintf(msg, "Choose Produce power");
+			break;
+
+		/* Choose card to discard in order to produce */
+		case CHOICE_DISCARD_PRODUCE:
+
+			/* Create prompt */
+			sprintf(msg, "Choose discard to produce");
+			break;
+
+		/* Choose search category */
+		case CHOICE_SEARCH_TYPE:
+
+			/* Create prompt */
+			sprintf(msg, "Choose Search category");
+			break;
+
+		/* Choose whether to keep searched card */
+		case CHOICE_SEARCH_KEEP:
+
+			/* Save special card */
+			num_special_cards = 1;
+			special_cards[0] = &g->deck[arg1];
+
+			/* Create prompt */
+			sprintf(msg, "Choose to keep/discard %s", g->deck[arg1].d_ptr->name);
+			break;
+
+		/* Choose color of Alien Oort Cloud Refinery */
+		case CHOICE_OORT_KIND:
+
+			/* Create prompt */
+			sprintf(msg, "Choose Alien Oort Cloud Refinery kind");
+			break;
+
+		/* Error */
+		default:
+			display_error("Unknown choice type!\n");
+			exit(1);
+	}
 	
 	/* Export the game */
 	export(g, who);
+	
+	/* Clear the number of special cards */
+	num_special_cards = 0;
 
 	/* Read the current choice position */
 	current = g->p[who].choice_pos;
