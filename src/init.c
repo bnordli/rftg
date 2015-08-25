c864dcaa967ce4bb7e9f002a18a69466c945b888
B. Nordli <bnordli@gmail.com>
2015-08-25 18:13:41 +0200

    Update header information

--- src/init.c
+++ src/init.c
@@ -3,6 +3,8 @@
  *
  * Copyright (C) 2009-2015 Keldon Jones
  *
+ * Source file modified by B. Nordli, August 2015.
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
--- src/net.c
+++ src/net.c
@@ -1,7 +1,7 @@
 /*
  * Race for the Galaxy AI
- *
- * Copyright (C) 2009-2015 Keldon Jones
+ * 
+ * Copyright (C) 2009-2011 Keldon Jones
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
